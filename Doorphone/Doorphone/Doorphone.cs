6c91e0db4394cb554e391eeaf3603d1a4d02657a
Northern Light <33227564+N0rthernL1ght@users.noreply.github.com>
2018-08-16 22:55:23 +0700

    Add files via upload

---
+++ Doorphone/Doorphone/Doorphone.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doorphone
+{
+    class Doorphone
+    {
+        private
+            int password = 123;
+        
+        public void Watchpas(int w)
+        {
+            if (password == w) Console.WriteLine("Welcome");
+            else Console.WriteLine("Password invalid");
+        }
+
+        public void Say(int p)
+        {
+            if (p == 1)
+            {
+                Console.WriteLine("It is Nikita Kochtygov");
+                Console.WriteLine("One second");
+                Console.WriteLine("oh, Welcome in your new home");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("It is police");
+                Console.WriteLine("But nobody call police");
+                Console.WriteLine("Goodbye. Go home");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ Doorphone/Doorphone/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doorphone
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int input;
+            int password=0;
+            int b;
+            Doorphone dp = new Doorphone();
+            
+            //Doorphone dp = new Doorphone();
+            while (password!=a){
+                //Doorphone dp = new Doorphone();
+                Console.WriteLine("Выберите действие: 1-ввести пароль 2 - позвонить вахтеру");
+                input = Convert.ToInt32(Console.ReadLine());
+                
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Enter password");
+                        password = Convert.ToInt32(Console.ReadLine());
+                        dp.Watchpas(password);
+                        break;
+                    case 2:
+                        Console.WriteLine("Who a you?");
+                        Console.WriteLine("Your choice: 1 - true 2 - lie");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        dp.Say(b);
+                        break;
+                    default:
+                        Console.WriteLine("Выберите действие: 1-ввести пароль 2 - позвонить вахтеру");
+                        break;
+                }
+            } 
+        
+            Console.ReadKey();
+
+        }
+    }
+}
